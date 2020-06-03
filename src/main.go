0a31e3ae5c439b02cb499bd3ee8df163c89f81a6
Michele Rexha <rexha.michele@gmail.com>
2020-06-03 20:06:46 +0200

    first commit

---
+++ src/main.go
@@ -0,0 +1,69 @@
+package neo4jClient
+
+import (
+	"fmt"
+	neo4j "github.com/neo4j/neo4j-go-driver/neo4j"
+)
+type Record map[string]interface{}
+
+type Client interface {
+	 Write(string, func(Record) error) error
+	 Read(string, func(Record) error) error
+}
+
+type client struct {
+	BoltUrl string
+	Username string
+	Password string
+}
+
+func NewClient(boltUrl string, username string, password string) client{
+	return client{
+		 boltUrl,
+		 username,
+		 password,
+	}
+}
+
+func (c client) Write(query string, job func(record Record) error) error {
+	return c.execute(neo4j.AccessModeWrite, query, job)
+}
+
+func (c client) Read(query string, job func(record Record) error) error {
+	return c.execute(neo4j.AccessModeRead, query, job)
+}
+
+func (c client) execute(accessMode neo4j.AccessMode, query string, job func(record Record) error) error {
+	driver, err := neo4j.NewDriver("bolt://" + c.BoltUrl, neo4j.BasicAuth(c.Username, c.Password, ""))
+	if err != nil {
+		return err // handle error
+	}
+	defer driver.Close()
+
+	session, err := driver.Session(accessMode)
+	if err != nil {
+		return err
+	}
+	defer session.Close()
+	result, err := session.Run(query, map[string]interface{}{})
+	if err != nil {
+		return err // handle error
+	}
+	for result.Next() {
+		err = result.Err()
+		if err != nil {
+			return err
+		}
+		record := Record{}
+		for _, key := range result.Record().Keys() {
+			record[key], _ = result.Record().Get(key)
+			fmt.Println(record[key])
+		}
+
+		if err := job(record); err != nil {
+			fmt.Println("Error", err)
+			return err
+		}
+	}
+	return nil
+}
